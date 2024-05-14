2de9f12efca5d3bc44214d81ba7d568f8f572cbf
santhoshm088 <115387017+santhoshm088@users.noreply.github.com>
2024-05-14 14:09:31 +0530

    first1

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -21,13 +21,13 @@ userRouter.get(
         
         name: ele.name,
         email: ele.email,
-        password: ele.password,
-        rollno: ele.rollno,
         loginTime: date,
       };
       arr.push(user);
+      
     });
     if (users) {
+      console.log(arr)
       res.send(arr);
       return;
     }
@@ -35,48 +35,115 @@ userRouter.get(
   })
 );
 
+// userRouter.put(
+//   '/signin',
+//   expressAsyncHandler(async (req, res) => {
+//     const users = await User.findOne({
+//       email: req.body.email,
+//       password:req.body.password
+      
+//     });
+   
+//     if (users) {
+      
+//       res.send({
+       
+//         name: users.name,
+//         email: users.email,
+        
+        
+//         token: generateToken(users),
+//       });
+//       return;
+//     }
+  
+//     const user = new User({
+      
+//       name: req.body.name,
+//       email: req.body.email,
+//       password: req.body.password,
+      
+//     });
+    
+//     const data = await user.save();
+    
+//     res.send({
+     
+//       name: data.name,
+//       email: data.email,
+      
+     
+//       token: generateToken(data),
+//     });
+    
+//     return;
+//   })
+// );
+
+
+
 userRouter.put(
   '/signin',
   expressAsyncHandler(async (req, res) => {
-    const users = await User.findOne({
-      email: req.body.email,
+    try {
+      const user = await User.findOne({ email: req.body.email });
       
-    });
-   
-    if (users) {
-      
-      res.send({
-       
-        name: users.name,
-        email: users.email,
-        password: users.password,
-        
-        token: generateToken(users),
-      });
-      return;
+      if (user && user.password === req.body.password) {
+        res.status(200).json({
+          name:user.name,
+          email: user.email,
+          token: generateToken(user),
+        });
+      } else {
+        res.status(404).json({ message: "Invalid email or password" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
     }
-  
-    const user = new User({
-      
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      
-    });
-    
-    const data = await user.save();
-    
-    res.send({
-     
-      name: data.name,
-      email: data.email,
-      password: data.password,
-     
-      token: generateToken(data),
-    });
-    
-    return;
   })
 );
+
+
+
+userRouter.post('/signup', async (req, res) => {
+
+   
+  const users = await User.findOne({
+    email: req.body.email,
+    
+  });
+
+  const data =
+  {
+  name:req.body.name,
+  email: req.body.email,
+  password: req.body.password,
+  }
+  
+ console.log(users);
+ try{
+  if(users)
+      {
+         
+          res.json("exist")
+      }
+      else
+      {
+        res.json("kjjhj")
+      
+        await User.insertMany(data)
+    }
+ }
+ 
+ catch(e)
+ {
+      console.log(e);
+ }
+
+});
+
+
+
+
 
 export default userRouter;
